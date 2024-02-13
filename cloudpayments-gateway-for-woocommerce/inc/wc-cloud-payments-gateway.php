b2092fdaf6e300b756ca6a7d939de892c5514915
Nick Ivanter <nick@emcraft.com>
2024-02-13 12:50:30 +0100

    Streamline error handling when token payment fails

--- cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
+++ cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
@@ -821,14 +821,19 @@ class WC_CloudPayments_Gateway extends WC_Payment_Gateway
                 $order->payment_complete();
                 $order->add_order_note(sprintf('Payment approved (TransactionID: %s)', $response['body']['Model']['TransactionId']));
                 return;
-            } else {
-				$pattern = '(ReasonCode: %d), %s ';
-				$order->add_order_note(wp_sprintf($pattern, $response['body']['Model']['ReasonCode'], $response['body']['Model']['Reason']));
-			}
+            }
+
+            if ($response['body']['Model']['ReasonCode']) {
+                $pattern = '(ReasonCode: %d), %s ';
+                $order->add_order_note(wp_sprintf($pattern, $response['body']['Model']['ReasonCode'], $response['body']['Model']['Reason']));
+            }
+
+            WC_Subscriptions_Manager::process_subscription_payment_failure_on_order($order->get_id());
 
             if ($response['body']['Message'] != null || $response['body']['Model']['CardHolderMessage'] != null) {
-                WC_Subscriptions_Manager::process_subscription_payment_failure_on_order($order->get_id());
-                $order->update_status('failed', sprintf(__('Error: %s', 'woocommerce'), $response['body']['Message']));
+
+                $order->update_status('failed', sprintf(__('Error: %s', 'woocommerce'),
+                    $response['body']['Message'] . ' ' . $response['body']['Model']['CardHolderMessage']));
             }
             
         }
